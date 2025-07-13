670f6b0b9c62ee0771b9b8aed627da9d81521c09
Aadarsh125 <sahuaadarsh1258@gmail.com>
2025-07-13 12:13:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"// treat as JS code as newer version
+
+//alert(3+3) //we are using node.js , not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+console.log("Aadarsh");
+
+let name = "Aadarsh"
+let age = 20
+let isLoggedIn = false
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
+
+
+
