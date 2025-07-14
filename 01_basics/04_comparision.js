91f0882b6fe4f4597e3659aa9f37bd0d3c25e45c
Aadarsh125 <sahuaadarsh1258@gmail.com>
2025-07-14 13:57:48 +0000

    cmparision and data type summaty

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// Basic Comversion
+// console.log(2 == 1);
+// console.log(2 >= 2);
+// console.log(2 <= 2);
+
+// different datatype
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//// Avoid this type of conversion
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// console.log(undefined == 0); 
+// console.log(undefined > 0); 
+// console.log(undefined < 0); 
+
+
+// === (Strick Check)
+
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,34 @@
+// Primitive DT
+
+// 7 types : String, Number, Boolean, null, Undefined, Symbol, BigInt  
+
+// const score = 100
+// const scoreValue = 100.4
+
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id === anotherid); // reture value are not same
+
+// const bigNumber = 231561451456464n//BigInt
+
+
+// Reference (Non Primitive)
+// Array, Object, Function
+
+const heros = ["superman", "spiderman", "ironman"]
+
+let myObj = {
+    name: "Aadarsh",
+    age: 20,
+}
+
+const myFun = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof anotherid);
