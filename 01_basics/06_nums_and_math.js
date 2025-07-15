89d358b6ee0726877abd7caab401ab785e9a78ef
Aadarsh125 <sahuaadarsh1258@gmail.com>
2025-07-15 13:48:39 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);// batata hai kitne character hai
+
+// console.log(balance.toFixed(1));// point ke baad ek value show krta hai
+
+// const otherNumbr = 123.8987
+// console.log(otherNumbr.toPrecision(4));// output 123.9 // 4 tk hi value batata hai
+
+// const hunderds = 1000000
+// console.log(hunderds.toLocaleString('en-IN'));//10,00,000 (,) laga deta hai
+
+// console.log(Math.abs(-4));// negative value ko positive me change krta hai
+// console.log(Math.round(4.3));// point ke baad wai value hata dega
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+ 
+// console.log(Math.abs(-4));
+
+// console.log(Math.min(4,3,5,6,76));
+// console.log(Math.max(4,3,5,6,76));
+
+console.log((Math.random()*10)+1);//1-10 ke beech me aayegi +1 laag denge to ab 0 value nhi ayegi
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+    console.log(Math.floor(Math.random() * (max - min + 1))+ min); // ab ye value hume 10 se 20 ke beech me dega
+    
