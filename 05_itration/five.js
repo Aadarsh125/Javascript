849774f418cbe84a587d618d3bbe519056102e49
Aadarsh125 <sahuaadarsh1258@gmail.com>
2025-09-25 14:14:51 +0000

    three update and four and five

---
+++ 05_itration/five.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printME)
+
+coding.forEach((item, index, arr)=> {
+    // console.log(item, index, arr);
+    
+})
+
+const myCoding = [
+    {
+        languguageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languguageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languguageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageFileName);
+    
+})
+
